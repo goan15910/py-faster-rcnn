ccfe1d8e269786fa471018b0c05632ad30a4aff6
Jeff King <goan15910@cml23.csie.ntu.edu.tw>
2016-10-07 15:46:52 +0800

    Add imagenet vid into factory.py

--- lib/datasets/factory.py
+++ lib/datasets/factory.py
@@ -11,6 +11,7 @@ __sets = {}
 
 from datasets.pascal_voc import pascal_voc
 from datasets.coco import coco
+from datasets.imagenet import imagenet
 import numpy as np
 
 # Set up voc_<year>_<split> using selective search "fast" mode
@@ -30,6 +31,11 @@ for year in ['2015']:
     for split in ['test', 'test-dev']:
         name = 'coco_{}_{}'.format(year, split)
         __sets[name] = (lambda split=split, year=year: coco(split, year))
+
+# Set up vid
+for split in ['train', 'val']:
+    name = 'imagenet_{}'.format(split)
+    __sets[name] = (lambda split=split: imagenet(split))
 
 def get_imdb(name):
     """Get an imdb (image database) by name."""
